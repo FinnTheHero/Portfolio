b40396610a9a16b264882fedfa6f4033b0339ce1
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-02-02 18:29:43 +0400

    Added Vercel analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aleksandre-nozadze.vercel.app/",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [
     tailwind(),
     sitemap({
